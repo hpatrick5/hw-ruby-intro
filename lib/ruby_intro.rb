2a9e6db8a6c35755c323ad4a53350b3342b65080
EC2 Default User <ec2-user@ip-172-31-21-108.us-east-2.compute.internal>
2021-01-30 01:05:09 +0000

    getters and setters

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -31,5 +31,15 @@ end
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = price
+  end
+  
+  attr_reader :isbn
+  attr_reader :price
+  
+  attr_writer :isbn
+  attr_writer :price
+  
 end
